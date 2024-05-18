98dd54188c67573ebb64bbd9357c785e044732dc
Ikuya Yamada <dalyson.roman34@gmail.com>
2024-05-19 03:19:50 +0900

    20240518

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,13 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0 {
+        1
+    } else {
+    let mut ans = 1;
+    for i in 1..=n {
+        ans *= i;
+    } 
+return ans}
 }
 
 #[cfg(test)]
