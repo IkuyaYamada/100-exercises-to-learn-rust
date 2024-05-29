6be2c6101988a460a23a01ca5c70e4174b8c4570
Ikuya Yamada <dalyson.roman34@gmail.com>
2024-05-30 00:57:08 +0900

    drop trait

--- exercises/04_traits/11_copy/src/lib.rs
+++ exercises/04_traits/11_copy/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+#[derive(PartialEq, Copy, Clone, Debug)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -9,6 +10,15 @@ impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
     }
+}
+use std::ops::Add;
+impl Add<Self> for WrappingU32 {
+    type Output = WrappingU32;
+    fn add(self, rhs: WrappingU32) -> WrappingU32 {
+        let num = self.value + rhs.value;
+        WrappingU32::new(num)
+    }
+
 }
 
 #[cfg(test)]
